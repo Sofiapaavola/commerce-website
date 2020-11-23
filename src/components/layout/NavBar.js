b2ca04e9a81e7e8da0693bbcb6fc11a97a91ec16
Sofiapaavola <paavola.sofia@gmail.com>
2020-11-23 18:54:17 +0000

    Added keys to product iteration and fontawesome into project for shopping cart implementation

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,10 +4,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-
-import { Link } from 'react-router-dom'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
+
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">sofsof's commerce site</Navbar.Brand>
@@ -21,6 +22,7 @@ export default function NavBar() {
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                 <Button variant="outline-info">Search</Button>
             </Form>
+                <Nav.Link href="/cart"><FontAwesomeIcon size='2x' color='white' icon={faShoppingCart}/></Nav.Link>
         </Navbar>
     )
 }
